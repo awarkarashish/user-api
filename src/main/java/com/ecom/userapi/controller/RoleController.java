569fb3a899e31bd0ba79f428f396f88972c6aa88
Ashish Awarkar <awarkarashish@gmail.com>
2019-12-08 12:23:57 +0530

    role controller changes

--- src/main/java/com/ecom/userapi/controller/RoleController.java
+++ src/main/java/com/ecom/userapi/controller/RoleController.java
@@ -1,5 +1,18 @@
 package com.ecom.userapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users/roles")
 public class RoleController {
+	
+	@GetMapping("/wel-come")
+	public ResponseEntity<String> welcome() {
+		return new ResponseEntity<>("welcome user-api", HttpStatus.OK);
+	}
 
 }
